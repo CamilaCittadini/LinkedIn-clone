08ea07428f1fdd30f6da081c1772d7a25051d56e
Camila Cittadini <cittadini.camilao@gmail.com>
2022-11-16 16:59:34 -0300

    fixed Avatar bug, retrieve session with getSession

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,23 @@ import Head from "next/head";
 import Image from "next/image";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import type { GetServerSideProps } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  {
+    /*checks if user is authenticated on the client side (status) and if not authenticated, pushes the user to home page */
+  }
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/home");
+    },
+  });
+
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6 ">
       <Head>
@@ -23,3 +38,20 @@ export default function Home() {
     </div>
   );
 }
+
+//retrieving user's session
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  //if there is no session (if user is not authenticated on the server side), the user will be redirected to the home page (otherwise we will retrieve the session)
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
